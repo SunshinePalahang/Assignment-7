42efc4f2d18b90f6f36b1856382ed9f48ba7a90a
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-29 15:58:42 +0800

    vowel list

--- program1.py
+++ program1.py
@@ -1,11 +1,14 @@
 #Create a program that ask for a sentence as input.
 #Display the number of words, vowels and consonants in the input 
+#ex.
+#input: Bahala kayo dyan
+#output:
+#words: 3
+#vowels: 6
+#consonants: 8
 
 sentence = input("Enter sentence: ")
 word_ = sentence.split()
+vowel_ = 0
+vowel_list =['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
 
-def calculateCount():
-    sentence_length = len(word_)
-
-
-calculate_ = calculateCount()
