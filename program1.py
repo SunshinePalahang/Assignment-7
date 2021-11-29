a813fd279fa5cc777f1b188dd796ebad1aae5b33
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-29 14:18:25 +0800

    user input sentence

---
+++ program1.py
@@ -0,0 +1,9 @@
+#Create a program that ask for a sentence as input.
+#Display the number of words, vowels and consonants in the input 
+
+def getSentence():
+    sentence = input("Enter sentence: ")
+
+
+#step1
+sentence_ = getSentence()
