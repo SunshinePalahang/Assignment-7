4e3a7c35ea85475ccf9e98f40bdf55b192e75b6c
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-29 15:27:04 +0800

    worg split

--- program1.py
+++ program1.py
@@ -2,3 +2,10 @@
 #Display the number of words, vowels and consonants in the input 
 
 sentence = input("Enter sentence: ")
+word_ = sentence.split()
+
+def calculateCount():
+    sentence_length = len(word_)
+
+
+calculate_ = calculateCount()
