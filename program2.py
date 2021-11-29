b63a0b512d5cde0c6bc2fb870015511ec1d04002
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-29 18:55:45 +0800

    import re

--- program2.py
+++ program2.py
@@ -7,27 +7,6 @@
 #ex.
 #input: P@ssw0rd+P@ssw0rd
 #ouput: Valid
-while True:
-    password = input("Create password: ")
-    valid = False
-    number = '0123456789'
-    capital_letter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    special_char = '!@#$%^&*()-_+={[]}|\/:;"<>,.?'
-    
-    
-    if not password in number:
-        print("Not valid ! It should contain one letter between [1-9]")
-        continue
-    elif not password in capital_letter:
-        print("Not valid ! It should contain one letter between [A-Z]")
-        continue 
-    elif not password in special_char:
-        print("Not valid ! It should contain at least one letter in [~!@#$%^&*]")
-        continue 
-    else:
-        valid=True
-        break
 
-if (valid):
-    print("Valid")
+import re
    
