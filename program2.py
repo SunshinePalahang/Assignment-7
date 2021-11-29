16192bfc14a317531194fb7a8a2bc1f787d3916c
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-29 16:28:33 +0800

    user input password

---
+++ program2.py
@@ -0,0 +1,11 @@
+#Create a program that check if password is valid
+#The password is valid if all criteria are met:
+#a. Greater than 15 letters
+#b. Have at least one capital letter
+#c. Have at least one number
+#d. Have at least one special char (!@#$%^&*()_+ etc)
+#ex.
+#input: P@ssw0rd+P@ssw0rd
+#ouput: Valid
+
+password = input("Create password: ")
