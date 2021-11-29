f111102a6101eb5a62d55f7c1c25de3c8f19411c
PALAHANG, Sunshine Richmer D <halimjonik@gmail.compal>
2021-11-29 15:21:40 +0800

    user input sentence

--- program1.py
+++ program1.py
@@ -1,18 +1,4 @@
 #Create a program that ask for a sentence as input.
 #Display the number of words, vowels and consonants in the input 
 
-def getSentence():
-    sentence = input("Enter sentence: ")
-    return sentence
-
-def calculateCount():
-    sentence_length = len(sentence_)
-    word_ = 1
-    vowel_ = 0
-    consonant_ = 0
-    vowel_list = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
-
-#step1
-sentence_ = getSentence()
-#step2
-calculate_ = calculateCount()
+sentence = input("Enter sentence: ")
